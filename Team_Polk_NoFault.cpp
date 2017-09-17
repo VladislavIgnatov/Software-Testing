e0bd4d30778786826f347bc849ef9d5f65b4118e
Vladislav Ignatov <vlad_isaacs2005@yahoo.com>
2017-09-17 16:12:00 -0400

    Update Team_Polk_NoFault.cpp

--- Team_Polk_NoFault.cpp
+++ Team_Polk_NoFault.cpp
@@ -1,6 +1,40 @@
 // Team Polk Members: Vladislav Ignatov, Leonard Holloman, Walter Soulliere, Timothy Jones
 #include <iostream>
+#include <string.h>
 using namespace std;
+
+// Displays student data, all of the class or just one student. READ ONLY. INDEPENDENT FUNCTION
+void DISPLAY() {
+	cout << "Display data\n";
+}
+
+// Write student data or change it
+void WRITE() {
+	DISPLAY();
+	cout << "Write data\n";
+}
+
+// Adds student to the database
+void ADD() {
+	cout << "ADD STUDENT\n";
+}
+
+// Delete student from database
+void DELETE() {
+	cout << "DELETE STUDENT\n";
+}
+
+// Saerch for a student
+void SEARCH() {
+	DISPLAY();
+	cout << "SEARCH for a Student\n";
+}
+
+// Saerch for a student
+void UPDATE() {
+	DISPLAY();
+	cout << "UPDATE a Students Info\n";
+}
 
 int main() {
 
@@ -8,27 +42,39 @@ int main() {
 
 	while (1) {
 		cout << "Welcome to USF Class-Roll System\n";
-		cout << "1: READ\n";
-		cout << "2: ADD\n";
-		cout << "3: DELETE\n";
-		cout << "4: SEARCH\n";
-		cout << "Enter in a number (1-4): ";
+		cout << "1: READ Student Data\n";
+		cout << "2: WRITE Student Data\n";
+		cout << "3: ADD Student\n";
+		cout << "4: DELETE Student\n";
+		cout << "5: SEARCH Student\n";
+		cout << "6: UPDATE Student\n";
+		cout << "7: EXIT\n";
+		cout << "Enter in a number (1-7): ";
 
 		cin >> choice;
 
 		switch (choice)
 		{
 		case(1):
-			cout << "Picked 1 \n";
+			DISPLAY();
 			break;
 		case(2):
-			cout << "Picked 2 \n";
+			WRITE();
 			break;
 		case(3):
-			cout << "Picked 3 \n";
+			ADD();
 			break;
 		case(4):
-			cout << "Picked 4 \n";
+			DELETE();
+			break;
+		case(5):
+			SEARCH();
+			break;
+		case(6):
+			UPDATE();
+			break;
+		case(7)://EXIT
+			return 0;
 			break;
 		default:
 			break;
