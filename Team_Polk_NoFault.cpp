3fcd478234e7063d3b80e534bfafcbd680c80adb
Vladislav Ignatov <vlad_isaacs2005@yahoo.com>
2017-09-23 00:29:53 -0400

    Update Team_Polk_NoFault.cpp

--- Team_Polk_NoFault.cpp
+++ Team_Polk_NoFault.cpp
@@ -1,21 +1,21 @@
 // Team Polk Members: Vladislav Ignatov, Leonard Holloman, Walter Soulliere, Timothy Jones
 #include <iostream>
-#include <string.h>
+#include <string>
 #include <fstream>
 #include <vector>
 using namespace std;
 
 class Student {
-	string name, email, unum;
-	int pres, proj, paper;
+	string name, unum, email;
+	int pres, paper, proj;
 public:
 	Student(string, string, string, int, int, int);
 	void set_name(string);
 	void set_unum(string);
 	void set_email(string);
 	void set_pres(int);
-	void set_proj(int);
 	void set_paper(int);
+	void set_proj(int);
 	string get_name() { return name; };
 	string get_unum() { return unum; };
 	string get_email() { return email; };
@@ -53,18 +53,22 @@ void Student::set_pres(int x)
 	pres = x;
 }
 
-void Student::set_proj(int x)
-{
-	proj = x;
-}
-
 void Student::set_paper(int x)
 {
 	paper = x;
 }
 
+void Student::set_proj(int x)
+{
+	proj = x;
+}
+
+vector<Student> database;
+
 // Displays student data, all of the class or just one student. READ ONLY. INDEPENDENT FUNCTION
 void DISPLAY() {
+	cout << "--------------Student Information--------------" << endl << endl;
+
 	for (int i = 0; i < database.size(); i++) {
 		cout << database[i].get_name() << "  " << database[i].get_unum() << "  " << database[i].get_email() << "  Presentaion: ";
 		cout << database[i].get_pres() << "  Paper: " << database[i].get_paper() << "  Project: " << database[i].get_proj() << endl;
@@ -74,43 +78,54 @@ void DISPLAY() {
 // Write student data or change it
 void WRITE() {
 	DISPLAY();
-	cout << "Write data\n";
+	cout << "Write data" << endl;
 }
 
 // Adds student to the database
 void ADD() {
-	cout << "ADD STUDENT\n";
-	Student tmp = Student("Not Sure", "notsure@charlesjr.com", "u96423678", 2, 2, 2);
-	database.push_back(tmp);
+	cout << "ADD STUDENT" << endl;
 }
 
 // Delete student from database
 void DELETE() {
-	cout << "DELETE STUDENT\n";
+	cout << "DELETE STUDENT" << endl;
 }
 
 // Saerch for a student
 void SEARCH() {
 	DISPLAY();
-	cout << "SEARCH for a Student\n";
+	cout << "SEARCH for a Student" << endl;
 }
 
 // Saerch for a student
 void UPDATE() {
 	DISPLAY();
-	cout << "UPDATE a Students Info\n";
+	cout << "UPDATE a Students Info" << endl;
 }
 
 int main() {
 	
 	string line;
+
 	ifstream db("database.txt");
 
 	if (db.is_open()) {//checks if it is open
 		while (getline(db, line))//grabs a line out of the document
 		{
-			// TODO read from the file and convert into vector class
-			Student tmp = Student("Not Sure", "u96423678", "notsure@charlesjr.com", 2, 4, 3);
+			string hold[6] = {};
+			char *next_token = NULL;
+			char *token = strtok_s((char*)line.c_str(), ",", &next_token);
+			int count = 0;
+
+			while (token) {
+				if (token != NULL)
+				{
+					hold[count++] = token;
+					token = strtok_s(NULL, ",", &next_token);
+				}
+			}
+			
+			Student tmp = Student(hold[0], hold[1], hold[2], stoi(hold[3]), stoi(hold[4]), stoi(hold[5]));
 			database.push_back(tmp);
 		}
 
@@ -124,11 +139,11 @@ int main() {
 		outfile.open("database.txt");
 
 		// Populating the file
-		outfile << "Rick Sanchaz, u01234567, regularrick@gmail.com, 6, 6, 6" << endl;
-		outfile << "Morty Smith, u76543210, morty@gmail.com, 3, 3, 2" << endl;
-		outfile << "Summer Smith, u88965238, summer@gmail.com, 4, 4, 3" << endl;
-		outfile << "Walter White, u96571368, hizenburg@gmail.com, 6, 6, 6" << endl;
-		outfile << "Jesse Pinkman, u79269858, pinkman@gmail.com, 1, 2, 1" << endl;
+		outfile << "Rick Sanchaz,u01234567,regularrick@gmail.com,6,6,6" << endl;
+		outfile << "Morty Smith,u76543210,morty@gmail.com,3,3,2" << endl;
+		outfile << "Summer Smith,u88965238,summer@gmail.com,4,4,3" << endl;
+		outfile << "Walter White,u96571368,hizenburg@gmail.com,6,6,6" << endl;
+		outfile << "Jesse Pinkman,u79269858,pinkman@gmail.com,1,2,1" << endl;
 
 		outfile.close();
 	}
@@ -136,7 +151,8 @@ int main() {
 	int choice = 0;
 
 	while (1) {
-		cout << "Welcome to USF Class-Roll System\n";
+		
+		cout << "\nWelcome to USF Class-Roll System\n";
 		cout << "1: READ Student Data\n";
 		cout << "2: WRITE Student Data\n";
 		cout << "3: ADD Student\n";
@@ -147,6 +163,8 @@ int main() {
 		cout << "Enter in a number (1-7): ";
 
 		cin >> choice;
+
+		cout << "\n";
 
 		switch (choice)
 		{
@@ -168,7 +186,7 @@ int main() {
 		case(6):
 			UPDATE();
 			break;
-		case(7)://EXIT
+		case(7):
 			return 0;
 			break;
 		default:
